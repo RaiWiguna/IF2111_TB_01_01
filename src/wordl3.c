fb7f6c421891f515cb446052d44267ad5c213857
sarahsurbakti2765 <sarahsrbkt2765@gmail.com>
2024-11-24 21:07:40 +0700

    update wordl3

--- src/wordl3.c
+++ src/wordl3.c
@@ -72,7 +72,15 @@ void playGame() {
     int attemptCount = 0;
     
     getSecretWord(secretWord);
-    printf("WELCOME TO W0RDL3, YOU HAVE 5 CHANCES TO ANSWER BEFORE YOU LOSE!\n\n");
+    printf("      888       888  .d88888b.  8888888b.  8888888b.  888      .d8888b.\n");
+    printf("      888   o   888 d88P   Y88b 888   Y88b 888   Y88b 888     d88P  Y88b \n");
+    printf("      888  d8b  888 888     888 888    888 888    888 888          .d88P \n");
+    printf("      888 d888b 888 888     888 888   d88P 888    888 888         8888  \n");
+    printf("      888d88888b888 888     888 8888888P   888    888 888           Y8b. \n");
+    printf("      88888P Y88888 888     888 888 T88b   888    888 888     888    888\n");
+    printf("      8888P   Y8888 Y88b. .d88P 888  T88b  888  .d88P 888     Y88b  d88P \n");
+    printf("      888P     Y888   Y88888P   888   T88b 8888888P   88888888  Y8888P   \n\n");
+    printf("-------WELCOME TO W0RDL3, YOU HAVE 5 CHANCES TO ANSWER BEFORE YOU LOSE!-------\n\n");
     printState(attempts, attemptCount);
 
     while (attemptCount < MAX_ATTEMPTS) {
