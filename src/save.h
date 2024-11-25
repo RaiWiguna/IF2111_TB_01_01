8a3852c6e3947d98ada7bde134d7057e520763cb
MZidniA <m.zidni.alkindi@gmail.com>
2024-11-25 09:48:52 +0700

    command save

---
+++ src/save.h
@@ -0,0 +1,11 @@
+#ifndef SAVE_COMMANDS_H
+#define SAVE_COMMANDS_H
+
+#include "mesinkata.h"
+#include "listlinear.h"
+
+void SAVE(char *namafile, TabInt T);
+/* menyimpan state ke dalam sebuah file yang terdapat pada folder save
+   I.S. : state belum disimpan
+   F.S. : state disimpan dalam file yg diberi nama sesuai dengan masukan dari pengguna dan dimuat di dalam folder save */
+#endif
