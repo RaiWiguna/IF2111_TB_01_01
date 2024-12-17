d62c744d5ed2435e3d3d803426fb89f99760ac48
sarahsurbakti2765 <sarahsrbkt2765@gmail.com>
2024-12-18 00:53:17 +0700

    Menambahkan mesin karakter

---
+++ adt/mesinkar.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "mesinkar.h"
+
+char currentChar;
+boolean EOP;
+static FILE *pita;
+static int retval;
+
+void START(){
+    pita =stdin;
+    ADV();
+}
+void START_File(FILE *file) {
+    /* Mesin siap dioperasikan. Pita disiapkan ... */
+    /* Algoritma */
+    pita = file;
+    ADV_File();
+}
+
+void ADV(){
+    retval = fscanf(pita, "%c", &currentChar);
+    EOP = (currentChar == EOF||currentChar=='\n');
+    if (EOP) {
+        currentChar ='\0';
+    }
+}
+
+void ADV_File() {
+    /* Pita dimajukan satu karakter. ... */
+    /* Algoritma */ 
+    retval = fscanf(pita, "%c", &currentChar);
+    EOP = (currentChar == MARKO);
+    if (EOP) {
+        fclose(pita);
+    }
+}
+
+char GetCC() {
+    /* Mengirimkan currentChar */
+    /* Algoritma */
+    return currentChar;
+}
+
+boolean IsEOP() {
+    /* Mengirimkan true jika currentChar = MARK */
+    /* Algoritma */
+    return EOP;
+}
---
+++ adt/mesinkar.h
@@ -0,0 +1,43 @@
+/* File: mesinkarakter.h */
+/* Definisi Mesin Karakter */
+
+#ifndef __MESIN_KAR_H_
+#define __MESIN_KAR_H_
+
+#include "boolean.h"
+#include <stdio.h>
+
+#define MARKO '\0'
+#define NEWLINE '\n'
+#define BLANK ' '
+/* State Mesin */
+extern char currentChar;
+extern boolean EOP;
+
+void START();
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   Pita baca diambil dari stdin.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
+void START_File(FILE *file);/*
+Membaca masukkan file, yang dimasukkan kedalam pita.
+*/
+void ADV();
+/* Pita dimajukan satu karakter.
+   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
+   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
+          currentChar mungkin = MARK
+          Jika  currentChar = MARK maka EOP akan menyala (true) */
+
+void ADV_File();
+
+char GetCC();
+/* Mengirimkan currentChar */
+
+boolean IsEOP();
+/* Mengirimkan true jika currentChar = MARK */
+
+#endif
